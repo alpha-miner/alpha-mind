bc595833c1121a2d0072ded619195301b5871197
wegamekinglc <scrappedprince.li@gmail.com>
2017-04-25 21:31:14 +0800

    modified to get better performance

--- alphamind/data/standardize.py
+++ alphamind/data/standardize.py
@@ -15,8 +15,15 @@ def standardize(x: np.ndarray, groups: np.ndarray=None) -> np.ndarray:
         df = pd.DataFrame(x)
         gs = df.groupby(groups)
 
-        mean_values = gs.transform(np.mean).values
-        std_values = gs.transform(np.std).values
+        mean_values = gs.mean()
+        std_values = gs.std().values
+
+        value_index = np.searchsorted(mean_values.index, groups)
+        mean_values = mean_values.values
+
+        mean_values = mean_values[value_index]
+        std_values = std_values[value_index]
+
         return (x - mean_values) / std_values
     else:
         return (x - x.mean(axis=0)) / x.std(axis=0)
--- alphamind/data/winsorize.py
+++ alphamind/data/winsorize.py
@@ -8,22 +8,38 @@ Created on 2017-4-25
 import pandas as pd
 import numpy as np
 
-
 def winsorize_normal(x: np.ndarray, num_stds: int=3, groups: np.ndarray=None) -> np.ndarray:
 
     if groups is not None:
         df = pd.DataFrame(x)
         gs = df.groupby(groups)
 
-        mean_values = gs.transform(np.mean).values
-        std_values = gs.transform(np.std).values
+        mean_values = gs.mean()
+        std_values = gs.std().values
+
+        value_index = np.searchsorted(mean_values.index, groups)
+        mean_values = mean_values.values
+
+        ubound = mean_values + num_stds * std_values
+        lbound = mean_values - num_stds * std_values
+
+        ubound = ubound[value_index]
+        lbound = lbound[value_index]
     else:
         std_values = x.std(axis=0)
         mean_values = x.mean(axis=0)
 
-    ubound = mean_values + num_stds * std_values
-    lbound = mean_values - num_stds * std_values
-    res = np.where(x > ubound, ubound, x)
-    res = np.where(res < lbound, lbound, res)
+        ubound = mean_values + num_stds * std_values
+        lbound = mean_values - num_stds * std_values
+
+    res = np.where(x > ubound, ubound, np.where(x < lbound, lbound, x))
 
     return res
+
+
+if __name__ == '__main__':
+    x = np.random.randn(3000, 10)
+    groups = np.random.randint(20, 40, size=3000)
+
+    for _ in range(1000):
+        winsorize_normal(x, 2, groups)
