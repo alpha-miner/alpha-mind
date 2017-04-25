324f7d513a1957ff5fbfe95252d5796e2922cfe3
wegamekinglc <scrappedprince.li@gmail.com>
2017-04-25 21:33:15 +0800

    added tests into test suite

--- alphamind/tests/test_suite.py
+++ alphamind/tests/test_suite.py
@@ -11,12 +11,14 @@ add_parent_path(__file__, 3)
 
 from alphamind.tests.test_neutralize import TestNeutralize
 from alphamind.tests.test_standardize import TestStandardize
+from alphamind.tests.test_winsorize import TestWinsorize
 from alphamind.utilities import alpha_logger
 from alphamind.utilities import TestRunner
 
 
 if __name__ == '__main__':
     runner = TestRunner([TestNeutralize,
-                         TestStandardize],
+                         TestStandardize,
+                         TestWinsorize],
                         alpha_logger)
     runner.run()
