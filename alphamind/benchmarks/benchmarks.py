eb4a9adcd81b0fda7d7d7a6c6ba477bc6ac293b7
wegamekinglc <scrappedprince.li@gmail.com>
2017-04-26 09:28:01 +0800

    update benchmark

--- alphamind/benchmarks/benchmarks.py
+++ alphamind/benchmarks/benchmarks.py
@@ -15,7 +15,12 @@ from alphamind.benchmarks.winsorize import benchmark_winsorize_normal_with_group
 if __name__ == '__main__':
 
     benchmark_neutralize(3000, 10, 1000)
+    benchmark_neutralize(30, 10, 50000)
     benchmark_standardize(3000, 10, 1000)
     benchmark_standardize_with_group(3000, 10, 100, 30)
-    benchmark_winsorize_normal(3000, 10, 1000)
-    benchmark_winsorize_normal_with_group(3000, 10, 100, 30)
+    benchmark_standardize(30, 10, 50000)
+    benchmark_standardize_with_group(30, 10, 5000, 5)
+    benchmark_winsorize_normal(30, 10, 50000)
+    benchmark_winsorize_normal_with_group(30, 10, 5000, 5)
+    benchmark_winsorize_normal(30, 10, 50000)
+    benchmark_winsorize_normal_with_group(30, 10, 5000, 5)
