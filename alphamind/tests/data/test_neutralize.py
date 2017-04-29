cc4f39b3427fa491a8b0bc852acba78e5d38ea35
wegamekinglc <scrappedprince.li@gmail.com>
2017-04-29 23:17:08 +0800

    added test for grouped neutralize

--- alphamind/tests/data/test_neutralize.py
+++ alphamind/tests/data/test_neutralize.py
@@ -27,6 +27,21 @@ class TestNeutralize(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(calc_res, exp_res)
 
+    def test_neutralize_with_group(self):
+        y = np.random.randn(3000, 4)
+        x = np.random.randn(3000, 10)
+        groups = np.random.randint(30, size=3000)
+
+        calc_res = neutralize(x, y, groups)
+
+        model = LinearRegression(fit_intercept=False)
+        for i in range(30):
+            curr_x = x[groups == i]
+            curr_y = y[groups == i]
+            model.fit(curr_x, curr_y)
+            exp_res = curr_y - curr_x @ model.coef_.T
+            np.testing.assert_array_almost_equal(calc_res[groups ==i ], exp_res)
+
 
 if __name__ == '__main__':
     unittest.main()
