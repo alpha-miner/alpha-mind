b4f2524643efdd4c5d468b562ed1675b7beed0ca
wegamekinglc <scrappedprince.li@gmail.com>
2017-05-15 18:22:12 +0800

    used relative risk measure

--- alphamind/portfolio/linearbuilder.py
+++ alphamind/portfolio/linearbuilder.py
@@ -25,7 +25,7 @@ def linear_build(er: np.ndarray,
     n, m = risk_exposure.shape
     w = cvxpy.Variable(n)
 
-    curr_risk_exposure = risk_exposure.T * (w - bm)
+    curr_risk_exposure = risk_exposure.T @ (w - bm)
 
     if not risk_target:
         risk_eq_target = np.zeros(m)
@@ -36,8 +36,8 @@ def linear_build(er: np.ndarray,
     else:
         constraints = [w >= lbound,
                        w <= ubound,
-                       curr_risk_exposure >= risk_target[0],
-                       curr_risk_exposure <= risk_target[1],
+                       curr_risk_exposure >= risk_target[0] * np.abs(risk_exposure.T @ bm),
+                       curr_risk_exposure <= risk_target[1] * np.abs(risk_exposure.T @ bm),
                        cvxpy.sum_entries(w) == 1.]
 
     objective = cvxpy.Minimize(-w.T * er)
