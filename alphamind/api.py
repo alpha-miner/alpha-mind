8da2de3f7c7aa5943515895310a089a5ed12e7ed
wegamekinglc <scrappedprince.li@gmail.com>
2017-08-16 17:03:26 +0800

    added risk styles to api

--- alphamind/api.py
+++ alphamind/api.py
@@ -11,11 +11,18 @@ from alphamind.analysis.quantileanalysis import quantile_analysis
 from alphamind.data.engines.universe import Universe
 from alphamind.portfolio.constraints import Constraints
 
+from alphamind.data.engines.sqlengine import risk_styles
+from alphamind.data.engines.sqlengine import industry_styles
+from alphamind.data.engines.sqlengine import macro_styles
+
 
 __all__ = [
     'SqlEngine',
     'factor_analysis',
     'quantile_analysis',
     'Universe',
-    'Constraints'
+    'Constraints',
+    'risk_styles',
+    'industry_styles',
+    'macro_styles'
 ]
