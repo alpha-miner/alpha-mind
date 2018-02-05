e9e7fc79b42c3e560e90ccd7a319d2d5de394972
wegamekinglc <scrappedprince.li@gmail.com>
2018-02-05 17:27:26 +0800

    1. added new constraints implementation; 2. bump version to 0.1.1

--- alphamind/tests/portfolio/test_constraints.py
+++ alphamind/tests/portfolio/test_constraints.py
@@ -7,7 +7,14 @@ Created on 2017-7-20
 
 import unittest
 import numpy as np
+import pandas as pd
 from alphamind.portfolio.constraints import Constraints
+from alphamind.portfolio.constraints import BoxBoundary
+from alphamind.portfolio.constraints import BoundaryImpl
+from alphamind.portfolio.constraints import BoundaryDirection
+from alphamind.portfolio.constraints import BoundaryType
+from alphamind.portfolio.constraints import create_box_bounds
+from alphamind.portfolio.constraints import LinearConstraints
 
 
 class TestConstraints(unittest.TestCase):
@@ -42,6 +49,100 @@ class TestConstraints(unittest.TestCase):
         risk_targets = cons.risk_targets()
         np.testing.assert_array_almost_equal(risk_targets[0], np.array([-0.1, -np.inf, -0.1]))
         np.testing.assert_array_almost_equal(risk_targets[1], np.array([0.1, np.inf, 0.1]))
+
+    def test_absolute_box_boundary(self):
+        lower = BoundaryImpl(BoundaryDirection.LOWER,
+                             BoundaryType.ABSOLUTE,
+                             -0.8)
+        upper = BoundaryImpl(BoundaryDirection.UPPER,
+                             BoundaryType.ABSOLUTE,
+                             1.1)
+        bound = BoxBoundary(lower, upper)
+
+        center = 2.2
+        l, u = bound.bounds(center)
+        self.assertAlmostEqual(l, 1.4)
+        self.assertAlmostEqual(u, 3.3)
+
+    def test_relative_box_boundary(self):
+        lower = BoundaryImpl(BoundaryDirection.LOWER,
+                             BoundaryType.RELATIVE,
+                             0.8)
+        upper = BoundaryImpl(BoundaryDirection.UPPER,
+                             BoundaryType.RELATIVE,
+                             1.1)
+        bound = BoxBoundary(lower, upper)
+
+        center = 2.2
+        l, u = bound.bounds(center)
+        self.assertAlmostEqual(l, 1.76)
+        self.assertAlmostEqual(u, 2.42)
+
+    def test_create_box_bounds_single_value(self):
+        names = ['a', 'b', 'c']
+        b_type = BoundaryType.RELATIVE
+        l_val = 0.8
+        u_val = 1.1
+
+        bounds = create_box_bounds(names,
+                                   b_type,
+                                   l_val,
+                                   u_val)
+
+        for key, bound in bounds.items():
+            l_bound = bound.lower
+            u_bound = bound.upper
+            self.assertEqual(l_bound.b_type, b_type)
+            self.assertEqual(u_bound.b_type, b_type)
+            self.assertAlmostEqual(l_bound.val, l_val)
+            self.assertAlmostEqual(u_bound.val, u_val)
+
+    def test_create_box_bounds_multiple_values(self):
+        names = ['a', 'b', 'c']
+        b_type = BoundaryType.RELATIVE
+        l_val = [0.9, 0.8, 1.1]
+        u_val = [1.1, 1.2, 1.3]
+
+        bounds = create_box_bounds(names,
+                                   b_type,
+                                   l_val,
+                                   u_val)
+
+        for i, name in enumerate(names):
+            bound = bounds[name]
+            l_bound = bound.lower
+            u_bound = bound.upper
+            self.assertEqual(l_bound.b_type, b_type)
+            self.assertEqual(u_bound.b_type, b_type)
+            self.assertAlmostEqual(l_bound.val, l_val[i])
+            self.assertAlmostEqual(u_bound.val, u_val[i])
+
+    def test_linear_constraints(self):
+        cons_mat = np.random.randn(100, 3)
+        backbone = np.random.randn(100)
+        names = ['a', 'b', 'c']
+        cons_mat = pd.DataFrame(cons_mat, columns=names)
+
+        b_type = BoundaryType.ABSOLUTE
+        l_val = -0.8
+        u_val = 1.1
+
+        bounds = create_box_bounds(names,
+                                   b_type,
+                                   l_val,
+                                   u_val)
+
+        constraints = LinearConstraints(bounds=bounds,
+                                        cons_mat=cons_mat,
+                                        backbone=backbone)
+
+        l_bounds, u_bounds = constraints.risk_targets()
+        risk_exp = constraints.risk_exp()
+
+        for i, name in enumerate(names):
+            center = risk_exp[:, i] @ backbone
+            self.assertAlmostEqual(center + l_val, l_bounds[i])
+            self.assertAlmostEqual(center + u_val, u_bounds[i])
 
 
 if __name__ == '__main__':
