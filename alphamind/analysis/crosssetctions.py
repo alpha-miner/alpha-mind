55e8b676ced47635555b5e4dc721753c3597952f
wegamekinglc <scrappedprince.li@gmail.com>
2018-03-24 10:23:16 +0800

    fixing miss match

--- alphamind/analysis/crosssetctions.py
+++ alphamind/analysis/crosssetctions.py
@@ -43,6 +43,7 @@ def cs_impl(ref_date,
     target_pos['weight'] = target_pos['weight'] / target_pos['weight'].abs().sum()
     target_pos = pd.merge(target_pos, dx_returns, on=['code'])
     target_pos = pd.merge(target_pos, total_data[['code'] + constraint_risk], on=['code'])
+    total_risk_exp = target_pos[constraint_risk]
     activate_weight = target_pos['weight'].values
     excess_return = np.exp(target_pos[['dx']].values) - 1.
     excess_return = factor_processing(excess_return, [winsorize_normal, standardize], total_risk_exp.values, [winsorize_normal, standardize]).flatten()
