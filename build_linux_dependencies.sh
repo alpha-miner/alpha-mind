fae6f50fdaa14fa19288ced0923c86613a8181f7
wegamekinglc <scrappedprince.li@gmail.com>
2020-03-12 16:41:49 +0800

    remove xgboost building steps

--- build_linux_dependencies.sh
+++ build_linux_dependencies.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 
-cd xgboost
-git submodule init
-git submodule update
-
-mkdir build
-cd build
-cmake ..
-make -j4
-cd ..
-
-cd python-package
-python setup.py install
-
-if [ $? -ne 0 ] ; then
-    cd ../..
-    exit 1
-fi
-
-cd ../..
-
 cd alphamind/pfopt
 
 git submodule init
--- build_windows_dependencies.bat
+++ build_windows_dependencies.bat
@@ -1,22 +1,5 @@
 @echo off
 
-cd xgboost
-git submodule init
-git submodule update
-mkdir build
-cd build
-cmake .. -G "Visual Studio 14 2015 Win64"
-msbuild xgboost.sln /m /p:Configuration=Release /p:Platform=x64
-
-if %errorlevel% neq 0 exit /b 1
-
-cd ../python-package
-python setup.py install
-
-if %errorlevel% neq 0 exit /b 1
-
-cd ../..
-
 cd alphamind\pfopt
 git submodule init
 git submodule update
