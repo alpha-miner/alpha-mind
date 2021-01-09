d0eb15930c9931394cf04a1288267a9df82bb905
wegamekinglc <scrappedprince.li@gmail.com>
2021-01-10 01:00:26 +0800

    DOC: bump version

--- setup.py
+++ setup.py
@@ -5,73 +5,11 @@ Created on 2017-4-25
 @author: cheng.li
 """
 
-import platform
 import io
-import os
 from setuptools import setup
 from setuptools import find_packages
-from Cython.Build import cythonize
-from distutils.extension import Extension
-import numpy as np
 
-VERSION = "0.2.6"
-
-if platform.system() != "Windows":
-    import multiprocessing
-    n_cpu = multiprocessing.cpu_count()
-else:
-    n_cpu = 0
-
-
-if platform.system() != "Windows":
-    extensions = [
-        Extension('alphamind.cython.optimizers',
-                  ['alphamind/cython/optimizers.pyx'],
-                  include_dirs=["alphamind/pfopt/include/clp",
-                                "alphamind/pfopt/include/ipopt",
-                                "alphamind/pfopt/include/pfopt",
-                                "alphamind/pfopt/include/eigen",
-                                "alphamind/pfopt/include/alglib"],
-                  libraries=['pfopt'],
-                  library_dirs=['alphamind/pfopt/lib'],
-                  extra_compile_args=['-std=c++11']),
-        ]
-
-    lib_files = []
-    lib_folder = 'alphamind/pfopt/lib'
-    for file in os.listdir(lib_folder):
-        lib_files.append(os.path.join(lib_folder, file))
-
-else:
-    extensions = [
-        Extension('alphamind.cython.optimizers',
-                  ['alphamind/cython/optimizers.pyx'],
-                  include_dirs=["alphamind/pfopt/include/clp",
-                                "alphamind/pfopt/include/ipopt",
-                                "alphamind/pfopt/include/pfopt",
-                                "alphamind/pfopt/include/eigen",
-                                "alphamind/pfopt/include/alglib"],
-                  libraries=['pfopt',
-                             'alglib',
-                             'libClp',
-                             'libCoinUtils',
-                             'libipopt',
-                             'libcoinhsl',
-                             'libcoinblas',
-                             'libcoinlapack',
-                             'libcoinmetis',
-                             'libcoinmumps'],
-                  library_dirs=['alphamind/pfopt/lib'],
-                  extra_compile_args=['/MD']),
-    ]
-
-    lib_files = []
-
-
-lib_files = []
-lib_folder = 'alphamind/pfopt/lib'
-for file in os.listdir(lib_folder):
-    lib_files.append(os.path.join(lib_folder, file))
+VERSION = "0.3.0"
 
 setup(
     name='Alpha-Mind',
@@ -83,9 +21,6 @@ setup(
     author_email='',
     scripts=['alphamind/bin/alphamind'],
     install_requires=io.open('requirements.txt', encoding='utf8').read(),
-    include_dirs=[np.get_include()],
-    ext_modules=cythonize(extensions),
-    data_files=[('alphamind/pfopt/lib', lib_files)],
     description='',
     include_package_data=True
 )
