add99815068e96ef7a924151eaa42323601294d4
wegamekinglc <scrappedprince.li@gmail.com>
2021-12-11 16:48:19 +0800

    enable passowrd

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ export DB_VENDOR="mysql"
 # export DB_URI="mysql+mysqldb://dxrw:dxRW20_2@121.37.138.1:13317/dxtest?charset=utf8"
 export DB_URI="mysql+mysqldb://reader:Reader#2020@121.37.138.1:13316/vision_product?charset=utf8"
 export FACTOR_TABLES="factor_momentum"
-jupyter lab --ip="0.0.0.0" --port=8080 --allow-root --NotebookApp.token='' --NotebookApp.password=''
+jupyter lab --ip="0.0.0.0" --port=8080 --allow-root --ServerApp.token='' --ServerApp.password='sha1:f7761f682bc4:1aba35e73699fe62570573de373bf95b491022a7'
